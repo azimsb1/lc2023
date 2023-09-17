016c108f45b78078ec744001200514584352ffae
azimsb1 <145243547+azimsb1@users.noreply.github.com>
2023-09-17 00:43:45 -0400

    Create valid-anagram.py

---
+++ valid-anagram.py
@@ -0,0 +1,39 @@
+# https://leetcode.com/problems/valid-anagram
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        # if lengths are different, can't be anagrams
+        if len(s) != len(t):
+            return False
+
+        # dict will count the freq of chars in s and t
+        count = defaultdict(int)
+
+        # for each char in s, increment the count
+        for char in s:
+            count[char] += 1
+        
+        # for each char in t, decrement the count
+        for char in t:
+            count[char] -= 1
+        
+        # if all counts are 0, that means same characters and frequencies
+        for val in count.values():
+            if val != 0:
+                return False
+        return True
+
+"""
+Time complexity: O(a+b)
+Space complexity: O(a+b)
+
+where a is the length of string s,
+  and b is the length of string t.
+"""
+
+# TODO: try this with just 2 for loops
