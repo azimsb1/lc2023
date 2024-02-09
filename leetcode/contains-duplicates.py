27a83416a9d8b32b1e28e67d25e306735c435c11
azimsb120 <145243547+azimsb120@users.noreply.github.com>
2024-02-09 00:46:55 -0500

    Update contains-duplicates.py

--- leetcode/contains-duplicates.py
+++ leetcode/contains-duplicates.py
@@ -1,4 +1,21 @@
-# https://leetcode.com/problems/contains-duplicate/
+'''
+LC: https://leetcode.com/problems/contains-duplicate/
+
+Approach:
+- maintain a "seen" hash set
+- for each number:
+    * if we have already seen it, return True
+    * otherwise, add it to the set in case we see it again in the future
+- at the end, return false. This means no num has been seen more than once
+
+Time complexity: O(n)
+- where n = len of nums
+- we are looking at each number at most once
+
+Space complexity: O(n)
+- where n = len of nums
+- in the worst case when all numbers are unique, we will store them all once in the seen hash set
+'''
 
 class Solution(object):
     def containsDuplicate(self, nums):
@@ -12,11 +29,3 @@ class Solution(object):
                 return True
             seen.add(num)
         return False
-
-'''
-Time complexity: O(n)
-where n = size of nums
-
-Space Complexity: O(n)
-where n = size of nums
-'''
